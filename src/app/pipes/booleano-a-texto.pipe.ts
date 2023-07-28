53f3fd3eac9ec1511a7cdb0ce6ed3cecb83b6535
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-07-27 22:26:08 -0300

    Update booleano-a-texto.pipe.ts

--- src/app/pipes/booleano-a-texto.pipe.ts
+++ src/app/pipes/booleano-a-texto.pipe.ts
@@ -1,12 +1,8 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { BooleanoATextoPipe } from './booleano-a-texto.pipe';
 
-@Pipe({
-  name: 'booleanoATexto'
-})
-export class BooleanoATextoPipe implements PipeTransform {
-
-  transform(valorBooleano: boolean, ...args: any[]): string {
-    return valorBooleano ? args[0] : args[1];
-  }
-
-}
+describe('BooleanoATextoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new BooleanoATextoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
