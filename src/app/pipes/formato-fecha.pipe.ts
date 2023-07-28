b98b34192f97e550b46746a352fbc21881e1cafc
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-07-27 22:25:26 -0300

    Update formato-fecha.pipe.ts

--- src/app/pipes/formato-fecha.pipe.ts
+++ src/app/pipes/formato-fecha.pipe.ts
@@ -4,13 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'formatoFecha'
 })
 export class FormatoFechaPipe implements PipeTransform {
-
-  transform(fecha: Date): string {
-    let resultado: string;
-
-    resultado = `${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()} a las ${fecha.getHours()}:${fecha.getMinutes()}` //dd/MM/YYYY a las HH:mm
-
-    return resultado;
+  transform(fecha: Date): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const resultado: string = `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()} a las ${fecha.getHours()}:${fecha.getMinutes()}`;
+        resolve(resultado);
+      }, 500); // Simulating a 500ms delay for the asynchronous operation
+    });
   }
-
 }
